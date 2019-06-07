5e0a89a2f8b833718972813fbf2182fb3531094c
mkhuthir <mkhuthir@gmail.com>
2019-06-07 11:48:41 +0400

    updated info

--- language/__x__/info.py
+++ language/__x__/info.py
@@ -2,8 +2,16 @@
 
 import cv2
 
-print("Version :",cv2.__version__)
-print("Files   :",cv2.__file__)
+print("\nPrinting info for cv2 module:....\n")
 
-print("Name    :",__name__)
+print("Package :",cv2.__package__)
+print("Name    :",cv2.__name__)
+print("Doc     :",cv2.__doc__)
+print("Version :",cv2.__version__)
+print("Path    :",cv2.__path__)
+print("Files   :",cv2.__file__)
+print("\n")
+
+
+
 
