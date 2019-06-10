c064e0606de88934f4eb54013b67263a1c17dbe2
mkhuthir <mkhuthir@gmail.com>
2019-06-10 22:17:17 +0400

    updated open3d

--- modules_imported/open3d/visualize.py
+++ modules_imported/open3d/visualize.py
@@ -5,7 +5,7 @@ import open3d as o3d
 
 print("Load a ply point cloud, print it, and render it")
 
-pcd = o3d.io.read_point_cloud("./data/fragment.pcd")
+pcd = o3d.io.read_point_cloud("./data/fragment.ply")
 
 print(pcd)
 print(np.asarray(pcd.points))
